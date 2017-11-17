117ff16beeb02e959105bef9b48fa233693e2a56
jitenderjs1998 <29479513+jitenderjs1998@users.noreply.github.com>
2017-11-17 22:32:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/websiteconverter/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.android.websiteconverter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView wv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        wv = (WebView) findViewById(R.id.wv);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.setFocusable(true);
+        wv.setFocusableInTouchMode(true);
+        wv.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+        wv.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        wv.getSettings().setDomStorageEnabled(true);
+        wv.getSettings().setDatabaseEnabled(true);
+        wv.getSettings().setAppCacheEnabled(true);
+        wv.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        wv.loadUrl("http://www.indiatoursonline.com/");
+        wv.setWebViewClient(new WebViewClient());
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (wv.canGoBack()) {
+            wv.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
